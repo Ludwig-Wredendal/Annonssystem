dca6fa8d88b5833865779959c92ff7d50d881c44
Ludwig-Wredendal <ludwig@wredendal.se>
2024-04-18 10:29:52 +0200

    Kan hämta prenumerant med prenumerationsnummer.

--- Controllers/AnnonsController.cs
+++ Controllers/AnnonsController.cs
@@ -4,6 +4,14 @@ namespace Annonssystem.Controllers
 {
     public class AnnonsController : Controller
     {
+        Uri baseAdress = new Uri("https://localhost:7234/api");
+        private readonly HttpClient _client;
+
+        public AnnonsController()
+        {
+            _client = new  HttpClient();
+            _client.BaseAddress = baseAdress;
+        }
         public IActionResult Index()
         {
             return View();
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,38 +2,61 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Web;
 
 namespace Annonssystem.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        Uri baseAdress = new Uri("https://localhost:7234/api");
+        private readonly HttpClient _client;
 
+        private readonly ILogger<HomeController> _logger; // nåt skräp för debugging.
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _client = new HttpClient();
+            _client.BaseAddress = baseAdress;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<PrenumerantDetalj> prenumerantLista = new List<PrenumerantDetalj>();
+            HttpResponseMessage response =  _client.GetAsync(_client.BaseAddress + "/Prenumerant/getPrenumeranter").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                prenumerantLista = JsonConvert.DeserializeObject<List<PrenumerantDetalj>>(data);
+            }
+            return View(prenumerantLista);
         }
+
+        // Hämtar en prenumerant via prenumerationsnummer.
+        public IActionResult GetPrenumerant(int prenumerationsnummer)
+        {
+            PrenumerantDetalj prenumerant = null;
+            HttpResponseMessage response = _client.GetAsync($"{_client.BaseAddress}/Prenumerant/GetPrenumerantByPn?prenumerationsnummer={prenumerationsnummer}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                prenumerant = JsonConvert.DeserializeObject<PrenumerantDetalj>(data);
+            }
+            return View(prenumerant);
+        }
+
 
         public IActionResult Privacy()
         {
             return View();
         }
 
-        
-        public IActionResult Annons()
-        {
-            return View();
-        }
 
-        //public int CheckPrenumerant()
-        //{
 
-        //}
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -41,6 +64,8 @@ namespace Annonssystem.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+
+        // Per video lösning:
         public async Task<IActionResult> testaGet()
         {
             List<PrenumerantDetalj> prenumerantlista = new List<PrenumerantDetalj>();
